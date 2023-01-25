7e7136dd3184f2dfa325a13e0cb200a316b7aa85
emilyjryan <emilyryanmac@gmail.com>
2023-01-25 09:04:10 -0800

    finished deliverable

--- src/components/ConstellationDetails.jsx
+++ src/components/ConstellationDetails.jsx
@@ -3,7 +3,8 @@ export default function ConstellationDetails(props) {
         <>
             <h3>Name: {props.constellation.name}</h3>
             <p>English name: {props.constellation.englishName}</p>
-            <p>Picture: {props.constellation.imageURL}</p>
+            <p>Picture:</p>
+            <img src={props.constellation.imageURL} alt={props.constellation.name} />
             <p>Hemisphere: {props.constellation.hemisphere}</p>
 
             <button onClick={props.handleShowFormClick}>Edit</button>
